e940508aca3703a57f716386859b4d32ab748dbe
Eric Morton <ericbm@yahoo.com>
2019-12-16 23:21:05 +0100

    cleaned up code and added clarifying comments

--- src/snowflakes.js
+++ src/snowflakes.js
@@ -1,10 +1,10 @@
 const snowflakes = {} // default export object
 const flakeSvgTemplates = [
+  // todo: integrate svg into script instead of relying on templates in html
   document.getElementById('flakeSvg1'),
   document.getElementById('flakeSvg2')
 ]
 const snowflakeCount = 100
-const rootNode = document.body
 const flakes = new Array(snowflakeCount).fill(null).map(createFlake)
 let enableAnimation = true
 let windFactor = Math.ceil(Math.random() * 2) - 1
@@ -13,40 +13,39 @@ const windCap = 2
 function createFlake () {
   const template = flakeSvgTemplates[Math.floor(Math.random() * flakeSvgTemplates.length)]
   const clone = document.importNode(template.content, true)
-  const flake = clone.querySelector('svg')
-  const size = Math.random() // 0-1
+  const flakeSvg = clone.querySelector('svg')
+  const size = Math.random() // 0 to 1
+  // between -5 and 105 to allow flakes slightly outside viewport
   const positionX = Math.random() * 110 - 5
   const positionY = Math.random() * 100 - 10
 
-  rootNode.appendChild(clone)
+  document.body.appendChild(clone)
 
-  flake.data = {
+  flakeSvg.classList.add('snowflake')
+  flakeSvg.style.height = (size * 30 + 10) + 'px'
+
+  return {
+    element: flakeSvg,
     x: positionX,
     y: positionY,
     size: size,
     velocity: (size + 0.4) / 3, // let smaller flakes move slower
     rotation: 0,
+    // rotate counter clockwise for negative values
     rotationVelocity: (Math.random() * 2) - 1
   }
-
-  flake.classList.add('snowflake')
-  flake.style.height = (size * 30 + 10) + 'px'
-
-  return flake
 }
 
 snowflakes.start = () => {
   window.requestAnimationFrame(step)
 }
 
-// todo: fix toggle (dunno why this not working)
 snowflakes.toggle = () => {
   (enableAnimation = !enableAnimation) && snowflakes.start()
 }
 
 function step () {
   if (!enableAnimation) return
-  // let 5 be strongest wind
   if (windFactor > windCap) {
     windFactor -= 0.1
   } else if (windFactor < -windCap) {
@@ -56,21 +55,23 @@ function step () {
     windFactor += (Math.random() / 10) - 0.05
   }
   flakes.forEach(flake => {
-    flake.data.x += windFactor * flake.data.velocity
-    if (flake.data.x > 107) flake.data.x = -6 // when clipped right side, move to left side
-    if (flake.data.x < -7) flake.data.x = 106 // and vice versa
+    // apply wind to each flake based on its velocity
+    flake.x += windFactor * flake.velocity
+    if (flake.x > 107) flake.x = -6 // when clipped right side, move to left side
+    if (flake.x < -7) flake.x = 106 // and vice versa
 
-    // fall down (bigger = quicker)
-    flake.data.y += flake.data.velocity
-    if (flake.data.y > 107) flake.data.y = 0
+    // fall down
+    flake.y += flake.velocity
+    if (flake.y > 107) flake.y = 0
 
     // rotate
-    flake.data.rotation += flake.data.rotationVelocity
-    if (flake.data.rotation > 360 || flake.data.rotation < -360) flake.data.rotation = 0
+    flake.rotation += flake.rotationVelocity
+    if (flake.rotation > 360 || flake.rotation < -360) flake.rotation = 0
 
     // apply styles
-    flake.style.transform = `translate(${flake.data.x}vw, ${flake.data.y}vh) rotate(${flake.data.rotation}deg)`
+    flake.element.style.transform = `translate(${flake.x}vw, ${flake.y}vh) rotate(${flake.rotation}deg)`
   })
+
   window.requestAnimationFrame(step)
 }
 
