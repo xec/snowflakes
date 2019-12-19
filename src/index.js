bfd058ae121fc78b6ee033f63a75ab729b8b48ce
Eric Morton <ericbm@yahoo.com>
2019-12-19 16:36:50 +0100

    moved svg resources to inline strings

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import snowflakes from './snowflakes'
 
 snowflakes.start()
 // document.getElementById('toggleFlakes').addEventListener('click', () => snowflakes.toggle())
+document.addEventListener('keypress', (e) => {
+  if (e.keyCode === 112) snowflakes.toggle()
+})
